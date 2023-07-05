1e5926e46459d5cb3db8e137dd9499f81a4aaff9
ardonplay <ardonplay@iMac-ardonplay.local>
2023-07-05 11:41:17 +0300

    final pre-redux version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './redux/state';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
